a1da732e313a2060372aef8c5fe589d882221618
Cyrof <73649950+Cyrof@users.noreply.github.com>
2022-05-07 01:59:10 +0800

    update

--- get-requirements.py
+++ get-requirements.py
@@ -4,4 +4,4 @@ import os
 os.system("pip install pipreqs")
 
 os.system("pipreqs ./ --encoding=utf-8 --ignore bin,etc,lib,lib64 --force")
-os.system("pip install -r requirements.txt")
+# os.system("pip install -r requirements.txt")
