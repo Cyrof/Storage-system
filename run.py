e0775f3e5d565ca07834e908c57bb5b8dfabdcb5
Cyrof <73649950+Cyrof@users.noreply.github.com>
2022-05-06 23:28:42 +0800

    update

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 import os
 
-
-os.system("pip install pipreqs")
-
-os.system("pipreqs ./ --encoding=utf-8 --ignore bin,etc,lib,lib64 --force")
-os.system("pip install -r requirements.txt")
+os.system('pip install -r requirements.txt')
