269b72ae7b668eaad3b92fa51b900289d8b8c37c
Cyrof <73649950+Cyrof@users.noreply.github.com>
2022-05-08 00:51:30 +0800

    update

--- create_db.py
+++ create_db.py
@@ -4,9 +4,12 @@ import secrets_folder
 new_db = mysql.connector.connect(
     host=secrets_folder.dbhost, # usually host='localhost' if you user root to create db
     user=secrets_folder.dbuser,
-    passwd=secrets_folder.dbpass
+    passwd=secrets_folder.dbpass,
+    database=secrets_folder.dbname
 )
 
 mycursor = new_db.cursor()
+
+mycursor.execute("show")
 
 # mycursor.execute('create database StorageSystem')
